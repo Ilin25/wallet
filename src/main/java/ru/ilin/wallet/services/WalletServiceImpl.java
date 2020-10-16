bf0411e0ea3a8517587be5737a97967dff22e8ae
Rustam Ilin <rustamilin2507@gmail.com>
2020-10-16 23:08:13 +0300

    Реализованы CRUD операции.Расчет баланса.Минимальный интерфейс.Исправлены ошибки в работе приложения.расчет баланса вынесен в отдельный сервис.

--- src/main/java/ru/ilin/wallet/services/WalletServiceImpl.java
+++ src/main/java/ru/ilin/wallet/services/WalletServiceImpl.java
@@ -3,20 +3,22 @@ package ru.ilin.wallet.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.ilin.wallet.models.Operation;
+import ru.ilin.wallet.repository.OperationRepository;
 
 @Service
 
 public class WalletServiceImpl implements WalletService {
-    private OperationService operationService;
-
+    private OperationRepository operationRepository;
     @Autowired
-    public WalletServiceImpl(OperationService operationService) {
-        this.operationService = operationService;
+    public WalletServiceImpl(OperationRepository operationRepository) {
+        this.operationRepository = operationRepository;
     }
+
+
 
     @Override
     public double getAccountBalance() {
-        double sum = operationService.getAllOperation().stream().mapToDouble(Operation::getTransactionAmount).sum();
+        double sum = operationRepository.findAll().stream().mapToDouble(Operation::getTransactionAmount).sum();
         return sum;
     }
 }
